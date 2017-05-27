29f57cc2513311a13b4d27a7844b01a46373f5b3
ricardo.grassi <ricardo.grassi@insidesistemas.com.br>
2017-05-27 10:58:28 -0300

    finish prototyping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,24 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadbarComponent,
-    SubMenuComponent
+    SubMenuComponent,
+    ProductListComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
