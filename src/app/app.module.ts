c1588ec2d944e7951ff333cc2555878ecf12be7e
ricardo.grassi <ricardo.grassi@insidesistemas.com.br>
2017-05-27 08:37:53 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent
   ],
   imports: [
     BrowserModule,
