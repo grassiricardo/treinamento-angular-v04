da703212f4134747e7a7b6d9d964cb9951dff409
ricardo.grassi <ricardo.grassi@insidesistemas.com.br>
2017-06-03 08:59:47 -0300

    validators

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,3 +1,4 @@
+import { CustomValidator } from './../../validators/custom.validator';
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
@@ -14,7 +15,8 @@ export class LoginPageComponent implements OnInit {
       email: ['', Validators.compose([
         Validators.minLength(5),
         Validators.maxLength(160),
-        Validators.required
+        Validators.required,
+        CustomValidator.EmailValidator
       ])],
       password: ['', Validators.compose([
         Validators.minLength(5),
@@ -25,6 +27,16 @@ export class LoginPageComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  checkEmail() {
+    this.form.controls['email'].disable;
+    document.getElementById('emailControl').classList.add('is-loading');
+    setTimeout(() => {
+      this.form.controls['email'].enable;
+      document.getElementById('emailControl').classList.remove('is-loading');
+      console.log(this.form.controls['email'].value) ;
+    }, 5000);
   }
 
 }
